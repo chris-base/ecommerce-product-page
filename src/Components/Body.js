cb26959bff9f5f49f1b802dd31a70bcea9c0cb84
Chris Base <baseappsdev@gmail.com>
2022-02-27 22:41:29 -0600

    finished responsive on mobile and fixed bugs

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,15 +1,65 @@
 import "../Styles/BodyStyles.css";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const Body = ({ setPhotoCarousel, currPhotoImgNumber, setCurrPhotoImgNumber, amountItemsInCart, setAmountItemsInCart }) => {
   const [itemCount, setItemCount] = useState(0);
   const [addCartScale, setAddCartScale] = useState(1);
+  const [under600px, setUnder600px] = useState(false);
+  // const [fromTopImgButton, setFromTopImgButton] = useState(0);
+
+  const photoRef = useRef(null);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 600) {
+        setUnder600px(true);
+      } else if (window.innerWidth > 600) {
+        setUnder600px(false);
+      }
+    };
+
+    if (window.innerWidth <= 600) {
+      setUnder600px(true);
+    } else if (window.innerWidth > 600) {
+      setUnder600px(false);
+    }
+
+    window.addEventListener("resize", handleResize);
+  }, []);
 
   return (
     <div id='Body'>
       <div id='photosContainer'>
-        <div id='mainPhotoContainer' onClick={() => setPhotoCarousel(true)}>
-          <img src={`image-product-${currPhotoImgNumber}.jpg`} id='mainPhoto' alt='product' />
+        <div id='mainPhotoContainer' onClick={() => (!under600px ? setPhotoCarousel(true) : {})}>
+          {under600px ? (
+            <div
+              className='nextImgButtonMobile'
+              style={{ left: "20px" }}
+              onClick={() => {
+                currPhotoImgNumber === 1 ? setCurrPhotoImgNumber(4) : setCurrPhotoImgNumber(currPhotoImgNumber - 1);
+              }}
+            >
+              <img src='icon-previous.svg' alt='' className='nextImgImgMobile' />
+            </div>
+          ) : (
+            <></>
+          )}
+
+          <img src={`image-product-${currPhotoImgNumber}.jpg`} id='mainPhoto' alt='product' ref={photoRef} />
+
+          {under600px ? (
+            <div
+              className='nextImgButtonMobile'
+              style={{ right: "20px" }}
+              onClick={() => {
+                currPhotoImgNumber === 4 ? setCurrPhotoImgNumber(1) : setCurrPhotoImgNumber(currPhotoImgNumber + 1);
+              }}
+            >
+              <img src='icon-next.svg' alt='' className='nextImgImgMobile' />
+            </div>
+          ) : (
+            <></>
+          )}
         </div>
 
         <div id='photoCollection'>
@@ -86,30 +136,28 @@ const Body = ({ setPhotoCarousel, currPhotoImgNumber, setCurrPhotoImgNumber, amo
             weather can offer.
           </p>
 
-          <div id='priceDiscountContainer'>
-            <p id='productPrice'>$125.00</p>
+          <div id='pricesContainer'>
+            <div id='priceDiscountContainer'>
+              <p id='productPrice'>$125.00</p>
 
-            <div id='discountContainer'>
-              <p id='discountText'>50%</p>
+              <div id='discountContainer'>
+                <p id='discountText'>50%</p>
+              </div>
             </div>
-          </div>
 
-          <p id='originalPrice'>$250.00</p>
+            <p id='originalPrice'>$250.00</p>
+          </div>
 
           <div id='quantityAddCartContainer'>
             <div id='quantityContainer'>
               <div className='quantityButton' onClick={() => (itemCount <= 0 ? null : setItemCount(itemCount - 1))}>
-                <p id='minusButton' className='quantityButtonText'>
-                  -
-                </p>
+                <img src='icon-minus.svg' className='quantityButtonImg' alt='' />
               </div>
 
               <p id='quantityText'>{itemCount}</p>
 
               <div className='quantityButton' onClick={() => setItemCount(itemCount + 1)}>
-                <p id='addButton' className='quantityButtonText'>
-                  +
-                </p>
+                <img src='icon-plus.svg' className='quantityButtonImg' alt='' />
               </div>
             </div>
 
